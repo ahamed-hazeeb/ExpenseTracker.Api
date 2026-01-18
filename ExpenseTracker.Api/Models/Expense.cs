90a76bd4dec28326e91f9a07cc7b75ad20e919cd
Hazeeb Marikar <hazeeb.marikar@discoverypartners.io>
2026-01-18 16:20:40 +0530

    Add project files.

---
+++ ExpenseTracker.Api/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.Api.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
