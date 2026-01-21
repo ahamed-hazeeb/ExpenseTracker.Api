55e0e82f989d2956ca9babc58eb3ca5ba605e968
Hazeeb Marikar <hazeeb.marikar@discoverypartners.io>
2026-01-21 13:36:13 +0530

    adding the Swagger to package to work

--- ExpenseTracker.Api/Program.cs
+++ ExpenseTracker.Api/Program.cs
@@ -8,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
@@ -18,6 +22,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
